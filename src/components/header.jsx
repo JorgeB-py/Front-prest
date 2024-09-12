99c04dfc73fdffb857f3d17e007d2aee64e48a28
JorgeB <j.bustamantep@uniandes.edu.co>
2024-09-12 10:50:46 -0500

    Lo que alcanzamos a hacer XD

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,46 +2,50 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-export default function Header() {
+export default function Header(font) {
 
-    const nav_links=[{
-        name:"Prest",
-        url:"/",
+    const nav_links = [{
+        name: "Prest",
+        url: "/",
     },
     {
-        name:"Soluciones",
-        url:"/soluciones",
+        name: "Soluciones",
+        url: "/soluciones",
     },
     {
-        name:"Nosotros",
-        url:"/nosotros",
+        name: "Nosotros",
+        url: "/nosotros",
     },
     {
-        name:"Recursos",
-        url:"/Recursos",
+        name: "Recursos",
+        url: "/Recursos",
 
-    },{
-        name:"Entra",
-        url:"/login"
+    }, {
+        name: "Entra",
+        url: "/login"
     },
     {
-        name:"Empieza ahora",
-        url:"/register"
+        name: "Empieza ahora",
+        url: "/register"
     }
     ]
     return (
-        <section className="header_index">
-        <nav class="nav nav-pills flex-column flex-sm-row">
-            {
-                nav_links.map((link,index)=>{
-                    return(
-                        <a key={index} class="flex-sm-fill text-sm-center nav-link" href={link.url}>{link.name}</a>
-                    )
-                })
-        }
-            
-            
-        </nav>
+        <section>
+
+            <nav className="nav nav-pills flex-column flex-sm-row">
+                {
+                    nav_links.map((link, index) => {
+                        return (
+                            <a key={index} class="flex-sm-fill text-sm-center nav-link" href={link.url}>{link.name}</a>
+                        )
+                    })
+                }
+                <a className="navbar-brand" href="/home">
+                    <img src="C:\Users\jorgi\Desktop\Uniandes\WEB\ISIS3710_202420_S2_E10_Front\src\img\Logo.png" alt="Logo" width="30" height="24" className="d-inline-block align-text-top" />
+                </a>
+
+
+            </nav>
         </section>
     );
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <React.StrictMode>
-        <Header />
+        <Header className="TWK"></Header>
     </React.StrictMode>
 );
