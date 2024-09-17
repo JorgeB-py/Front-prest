712cc328650df90dc0f8b8b0bd9a23fd92dfa493
JorgeB <j.bustamantep@uniandes.edu.co>
2024-09-17 11:49:09 -0500

    enrutamiento listo

---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Login() {
+    return(
+        <div>
+            <h1>Iniciar Sesión</h1>
+            <p>En esta sección encontrarás las soluciones que ofrecemos</p>
+        </div>
+    );
+}
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/header.css';
+import { Link } from "react-router-dom";
+
 
 export function Header() {
     const nav_links = [
-        { name: "Soluciones", url: "/soluciones" },
-        { name: "Nosotros", url: "/nosotros" },
-        { name: "Recursos", url: "/Recursos" },
+        { name: "Soluciones", url: "soluciones"},
+        { name: "Nosotros", url: "nosotros"},
+        { name: "Recursos", url: "recursos"},
     ];
 
     return (
@@ -14,9 +16,9 @@ export function Header() {
             <nav className="navbar navbar-expand-lg custom-navbar">
                 <div className="container-fluid">
                     {/* Logo */}
-                    <a className="navbar-brand custom-logo" href="/">
+                    <Link className="navbar-brand custom-logo" to="/">
                         <img src="/Logo_letras.png" alt="Logo" width="213" height="73" />
-                    </a>
+                    </Link>
 
                     {/* Botón para mobile */}
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -28,21 +30,24 @@ export function Header() {
                         <ul className="navbar-nav separation">
                             {nav_links.map((link, index) => (
                                 <li key={index} className="nav-item">
-                                    <a className="nav-link custom-links" href={link.url}>{link.name}</a>
+                                    <Link className="nav-link custom-links" to={link.url}>
+                                        {link.name}
+                                    </Link>
                                 </li>
                             ))}
                         </ul>
 
                         <ul className="navbar-nav ms-auto separation">
                             <li className="nav-item">
-                                <a className="nav-link custom-links" href="/login">Entra</a>
+                                <Link className="nav-link custom-links" to="login">Entra</Link>
                             </li>
                             <li className="nav-item">
-                                <a href="/registro" className=" buttom-empieza">Empieza ahora</a>
+                                <Link to="signup" className="buttom-empieza">Empieza ahora</Link>
                             </li>
                         </ul>
                     </div>
                 </div>
+                {/* Definición de las rutas */}
             </nav>
         </header>
     );
