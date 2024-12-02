5dc892c3c0bce9cb19a69837dc506d45df1ece96
maurive2014 <o.urregov@uniandes.edu.co>
2024-12-02 11:27:34 -0500

    Arreglo Cypress HU-7

--- cypress/e2e/HU-7.Spec.cy.js
+++ cypress/e2e/HU-7.Spec.cy.js
@@ -1,20 +1,10 @@
+import { waitFor } from "@testing-library/react";
+import { wait } from "@testing-library/user-event/dist/utils";
+
 describe('Pruebas para HU-7', () => {
-	const findDateInCalendar = (beforeButton, dateObj) => {
-		cy.get('.title-calendar').invoke('text').then((text) => { // Obtener el texto de .title-calendar
-			if (text === `${dateObj.month} ${dateObj.year}`) { // Comprabar si es el mes-año escogido
-				cy.contains('button', dateObj.day).click(); // Seleccionar el dia
-			}
-			else {
-				beforeButton.click(); //Devolvernos un mes
-				findDateInCalendar(beforeButton, dateObj); // Volver a buscar
-			}
-		})
-	}
-
-
 	beforeEach(() => {
 		// Acceder al componente principal
-		cy.visit('http://localhost:3000/deudorApp', {
+		cy.visit('http://localhost:3001/login',{
 			onBeforeLoad(win) {
 				Object.defineProperty(win.navigator, 'languages', {
 					value: ['en-US'],
@@ -23,50 +13,47 @@ describe('Pruebas para HU-7', () => {
 					value: 'en-US',
 				});
 			}
-		});
+		})
+		cy.get("input[id='username']").clear().type('prestamista');
+		cy.get("input[id='password']").clear().type('prestamista');
+		cy.contains('button', 'Login como Prestamista').click();
+		cy.contains('a', 'Information').click();
+		cy.wait(1000)
+		cy.get('button[aria-label="Show Details"]').eq(0).click();
 	});
+
+	// afterEach(() => {
+	// 	cy.clearLocalStorage();
+	// 	cy.clearCookies();
+	// 	cy.window().then((win) => {
+	// 	  win.sessionStorage.clear();
+	// 	});
+	//   });
+
+
 	it("Verificar que el las fechas de inicio, caducidad y la frecuencia de pago se puede editar y guardar", () => {
 		cy.contains('button', 'Edit info').click();
-		const startDate = cy.get('button[aria-label="Select a date"]').eq(0)
-		const dueDate = cy.get('button[aria-label="Select a date"]').eq(1)
-		let date;
+		cy.get('#fechaInicio').type('2023-12-01');
+		cy.get('#fechafin').type('2024-12-01');
+		cy.get('#prestado').clear().type('123123');
+		cy.get('#interes').clear().type('50');
 
-		startDate.click();
-		date = { day: "10", month: "January", year: "2023" }
-		findDateInCalendar(cy.get('button[aria-label="Before"]').click(), date);
-		startDate.click();
-
-		dueDate.click();
-		date = { day: "15", month: "June", year: "2023" }
-		findDateInCalendar(cy.get('button[aria-label="Before"]').click(), date);
-		dueDate.click();
-
-		cy.get('select[aria-label="Payment Frequency"').select('Biweekly')
-		cy.contains('button', 'Save').click();
+		cy.contains('button', 'Actualizar Datos').click();
+		cy.get('button[aria-label="Close"]').click();
 
 		// Comprabar los datos
-		cy.contains('January 10, 2023').should('be.visible');
-		cy.contains('June 15, 2023').should('be.visible');
-		cy.contains('Biweekly').should('be.visible');
+		cy.contains('2024-12-01').should('be.visible');
+		cy.contains('2023-12-01').should('be.visible');
+		cy.contains('123123').should('be.visible');
+		cy.contains('50').should('be.visible');
 	})
 
 	it("Verificar error en fechas incoherentes de inicio y vencimiento", () => {
 		cy.contains('button', 'Edit info').click();
-		const startDate = cy.get('button[aria-label="Select a date"]').eq(0)
-		const dueDate = cy.get('button[aria-label="Select a date"]').eq(1)
-		let date;
+		cy.get('#fechaInicio').type('2024-12-01');
+		cy.get('#fechafin').type('2023-12-01');
 
-		startDate.click();
-		date = { day: "15", month: "June", year: "2023" }
-		findDateInCalendar(cy.get('button[aria-label="Before"]').click(), date);
-		startDate.click();
-		
-		dueDate.click();
-		date = { day: "10", month: "January", year: "2023" }
-		findDateInCalendar(cy.get('button[aria-label="Before"]').click(), date);
-		dueDate.click();
-
-		cy.contains('button', 'Save').click();
+		cy.contains('button', 'Actualizar Datos').click();
 
 		cy.contains('Start and Due Dates must be consistent.').should('be.visible');
 	})
