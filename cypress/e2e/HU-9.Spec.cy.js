92f4b07c8c12fbf86bff63e666091035af3e4ef6
o.urregov <o.urregov@uniandes.edu.co>
2024-12-02 23:57:35 -0500

    arreglar H9 cypress

--- cypress/e2e/HU-9.Spec.cy.js
+++ cypress/e2e/HU-9.Spec.cy.js
@@ -1,65 +1,58 @@
 describe('Pruebas para HU-9', () => {
     beforeEach(() => {
-      // Acceder al componente principal
-      cy.visit('http://localhost:3000/infodeudor', {
-        onBeforeLoad(win) {
-          Object.defineProperty(win.navigator, 'languages', {
-            value: ['es-ES'],
-          });
-          Object.defineProperty(win.navigator, 'language', {
-            value: 'es-ES',
-          });
-        }
-      });
+      
+      cy.visit('http://localhost:3001/login',{
+			onBeforeLoad(win) {
+				Object.defineProperty(win.navigator, 'languages', {
+					value: ['en-US'],
+				});
+				Object.defineProperty(win.navigator, 'language', {
+					value: 'en-US',
+				});
+			}
+		  })
+
+      cy.get("input[id='username']").clear().type('prestamista');
+      cy.get("input[id='password']").clear().type('prestamista');
+      cy.contains('button', 'Login como Prestamista').click();
+      cy.contains('a', 'Information').click();
+      cy.wait(1000)
+
     });
   
     it('Debe cargar los datos iniciales del deudor correctamente', () => {
-      // Verificar el nombre del deudor
-      cy.contains('Armando Casas').should('exist');
       
       // Validar información personal
-      cy.contains('C.C. 1110450340').should('exist');
-      cy.contains('Edad: 24').should('exist');
-      cy.contains('hola@gmail.com').should('exist');
-      cy.contains('3143807270').should('exist');
+      cy.contains('C.C').should('exist');
+      cy.contains('Age:').should('exist');
+      cy.contains('Email:').should('exist');
+      cy.contains('Phone number:').should('exist');
       
       // Validar estado laboral
-      cy.contains('Empleado').should('exist');
-  
-      // Validar deudas
-      cy.contains('Estudios').should('exist');
-      cy.contains('200000').should('exist');
-      cy.contains('Carro').should('exist');
-      cy.contains('100000').should('exist');
+      cy.contains('Work Status:').should('exist');
+
     });
     it('Debe mostrar los íconos correctos en la información rápida', () => {
         cy.get('.custom-container .bi-currency-dollar').should('exist');
-        cy.get('.custom-container .bi-star').should('exist');
         cy.get('.custom-container .bi-briefcase').should('exist');
       });
   
     it('Debe permitir editar la información del deudor', () => {
       // Abrir modal de edición
       cy.get('.bi-pencil-square').click();
-      cy.get('.modal-title').should('contain', 'Editar información');
+      cy.get('.modal-title').should('contain', 'Editar Deudor');
   
       // Editar campos del formulario
       cy.get('input').eq(0).clear().type('Nuevo Nombre');
-      cy.get('input').eq(1).clear().type('123456789');
-      cy.get('input').eq(2).clear().type('Desempleado');
-      cy.get('input').eq(3).clear().type('30');
-      cy.get('input').eq(4).clear().type('3216549870');
-      cy.get('input').eq(5).clear().type('nuevoemail@gmail.com');
+      cy.get('input').eq(1).clear().type('nuevoemail@gmail.com');
+      cy.get('input').eq(2).clear().type('3143807270');
   
       // Guardar cambios
       cy.contains('Guardar').click();
   
       // Validar cambios en el componente
       cy.contains('Nuevo Nombre').should('exist');
-      cy.contains('123456789').should('exist');
-      cy.contains('Desempleado').should('exist');
-      cy.contains('Edad: 30').should('exist');
-      cy.contains('3216549870').should('exist');
+      cy.contains('3143807270').should('exist');
       cy.contains('nuevoemail@gmail.com').should('exist');
     });
   
@@ -73,7 +66,7 @@ describe('Pruebas para HU-9', () => {
   
     it('Debe permitir navegar a la página de creación de préstamos', () => {
       // Clic en el botón "Crear Préstamo"
-      cy.contains('Crear Préstamo').click();
+      cy.contains('Create Loan').click();
   
       // Verificar navegación a la página de préstamos
       cy.url().should('include', '/crearPrestamo');
